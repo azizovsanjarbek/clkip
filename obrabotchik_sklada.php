b2013595fa3dddd2d6eb82ef325d19a0b7178733
Sanjarbek Azizov <azizovlab@gmail.com>
2023-09-20 16:43:32 +0500

    Исправил и обновил код обновления прихода

--- obrabotchik_sklada.php
+++ obrabotchik_sklada.php
@@ -10,7 +10,7 @@ $db = mysqli_connect($localhost, $username, $password, $database);
  //// добавление новых данных в главную таблицу////////////
 //////////////////////////////////////////////////////////
 
-$data_upload = "SELECT * FROM date_of_upload WHERE upload_date='$today'";
+$data_upload = "SELECT * FROM date_of_upload WHERE upload_date='$today' and name_table LIKE 'main_table'";
 $data_upload_result = $db->query($data_upload );
 if($data_upload_result->num_rows > 0){
   echo "файл уже импортирован";
@@ -31,7 +31,7 @@ else{
       echo "Error: " . $add_sql . "<br>" . $db->error;
     }
    }
-   $add_upload_date="INSERT date_of_upload VALUES (NULL,'$today','$count_today_import')";
+   $add_upload_date="INSERT date_of_upload VALUES (NULL,'$today','$count_today_import','main_table')";
    if ($db->query($add_upload_date) === TRUE) {
      echo "New record created successfully <br><br>";
     }
@@ -43,70 +43,54 @@ else{
   ////////////////////////////////////////////////////////////
  /////////проверка на изменние данных вчера и сегодня////////
 ////////////////////////////////////////////////////////////
+$prihod_data_upload = "SELECT * FROM date_of_upload WHERE upload_date='$today' and name_table LIKE 'prihod' ";
+$prihod_data_upload_result = $db->query($prihod_data_upload );
+if($prihod_data_upload_result->num_rows > 0){
+
+  echo "Данные для прихода уже обновлены";
+}
+else{
 $prihod_contol = "SELECT * FROM importxslx";
 $prihod_contol_result= $db->query($prihod_contol);
 if($prihod_contol_result->num_rows > 0) {
 while($prihod_contol_row = $prihod_contol_result->fetch_assoc()) {
 
-$prihod_update = "SELECT * FROM main_table WHERE nomer = '$prihod_contol_row[nomer]' and cena LIKE '$prihod_contol_row[cena]' and data_pri LIKE '$prihod_contol_row[data_pri]' and sklad LIKE '$prihod_contol_row[sklad]' and data_create_zap LIKE '$prihod_contol_row[data_create_zap]' and pol_create_zap LIKE '$prihod_contol_row[pol_create_zap]' and UPLOAD_DATE >= '2023-09-19'";
+$prihod_update = "SELECT * FROM main_table WHERE nomer = '$prihod_contol_row[nomer]' and cena LIKE '$prihod_contol_row[cena]' and data_pri LIKE '$prihod_contol_row[data_pri]' and sklad LIKE '$prihod_contol_row[sklad]' and data_create_zap LIKE '$prihod_contol_row[data_create_zap]' and pol_create_zap LIKE '$prihod_contol_row[pol_create_zap]' and data_izm_zap LIKE '$prihod_contol_row[data_izm_zap]' and UPLOAD_DATE >= '2023-09-19'";
 $prihod_update_result = $db->query($prihod_update);
-if($prihod_update_result->num_rows == 3){
+if($prihod_update_result->num_rows == 2){
   echo "все хорошо <br/>";
 }
+elseif($prihod_update_result->num_rows == 1){
+
+  $add_prihod = "INSERT prihod VALUES (NULL,'$prihod_contol_row[kod_material]','$prihod_contol_row[name]','$prihod_contol_row[nomer]','$prihod_contol_row[kolichestvo]','$prihod_contol_row[ed_izm]','$prihod_contol_row[cena]','$prihod_contol_row[summa]','$prihod_contol_row[data_pri]','$prihod_contol_row[sklad]','$prihod_contol_row[data_create_zap]','$today')";
+  $db->query($add_prihod);
+  //echo "id ".$prihod_contol_row["nomer"]." name -".$prihod_contol_row["name"]." cena -".$prihod_contol_row["cena"]." data_pri -".$prihod_contol_row["data_pri"]." kto sozdal -".$prihod_contol_row["pol_create_zap"]." kto sozdal -".$prihod_contol_row["data_izm_zap"]."<br/>";
+}
 else{
-  echo "что то пошло не так <br/>";
+  echo "Ошибка ";// необходимо создать таблицу для ошибок
 }
 }
+
+
+$add_prihod_upload_date="INSERT date_of_upload VALUES (NULL,'$today','11','prihod')";
+if ($db->query($add_prihod_upload_date) === TRUE) {
+  echo "New record created successfully <br><br>";
+ }
+  else {
+    echo "Error: " . $add_prihod_upload_date . "<br>" . $db->error;
+  }
+
 }
 else{
   echo "Данные для прихода не удалось получить";
 }
+  
+}
+
+
 //echo "id: " . $prihod_contol_row["nomer"]. " - Name: " . $prihod_contol_row["name"]. " --- " . $prihod_contol_row["data_pri"].  " --- " . $prihod_contol_row["sklad"]. " --- " . $prihod_contol_row["cena"]."<br>";
 
 $db->close();
 
-/*
-$new = "DELET e1 FROM employees AS e1 INNER JOIN employees as e2 WHERE e1.id<e2.id  AND e1.email = e2.email";
 
-/* Попытка подключения к серверу MySQL. Предполагая, что вы используете MySQL
- сервер с настройкой по умолчанию (пользователь root без пароля)  
-$link = mysqli_connect("localhost", "root", "", "demo");
-
-// Проверка подключения
-if ($link === false) {
-    die("Ошибка подключения. " . mysqli_connect_error());
-}
-
-// Попытка выполнения запроса select
-$sql = "SELECT * FROM persons WHERE first_name='john'";
-if ($result = mysqli_query($link, $sql)) {
-    if (mysqli_num_rows($result) > 0) {
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>id</th>";
-        echo "<th>first_name</th>";
-        echo "<th>last_name</th>";
-        echo "<th>email</th>";
-        echo "</tr>";
-        while ($row = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['first_name'] . "</td>";
-            echo "<td>" . $row['last_name'] . "</td>";
-            echo "<td>" . $row['email'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Закрыть набор результатов
-        mysqli_free_result($result);
-    } else {
-        echo "Записей, соответствующих вашему запросу, не найдено.";
-    }
-} else {
-    echo "ОШИБКА: не удалось выполнить $sql. " . mysqli_error($link);
-}
-
-// Закрыть соединение
-mysqli_close($link);
-*/
 ?>
