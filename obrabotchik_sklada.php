ac6e156bc636d5e142f25c7544f99e5c3947b3e9
Sanjarbek Azizov <azizovlab@gmail.com>
2023-09-29 15:02:32 +0500

    работал над расходом

--- obrabotchik_sklada.php
+++ obrabotchik_sklada.php
@@ -3,6 +3,7 @@ include_once 'pass.php';
 mb_internal_encoding("UTF-8");
 
 $today=date("Y-m-d");
+$tomorrow = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")-1, date("Y")));
 
 $db = mysqli_connect($localhost, $username, $password, $database);
 
@@ -56,7 +57,7 @@ $prihod_contol_result= $db->query($prihod_contol);
 if($prihod_contol_result->num_rows > 0) {
 while($prihod_contol_row = $prihod_contol_result->fetch_assoc()) {
 
-$prihod_update = "SELECT * FROM main_table WHERE nomer = '$prihod_contol_row[nomer]' and cena LIKE '$prihod_contol_row[cena]' and data_pri LIKE '$prihod_contol_row[data_pri]' and sklad LIKE '$prihod_contol_row[sklad]' and data_create_zap LIKE '$prihod_contol_row[data_create_zap]' and pol_create_zap LIKE '$prihod_contol_row[pol_create_zap]' and data_izm_zap LIKE '$prihod_contol_row[data_izm_zap]' and UPLOAD_DATE >= '2023-09-20'";
+$prihod_update = "SELECT * FROM main_table WHERE nomer = '$prihod_contol_row[nomer]' and cena LIKE '$prihod_contol_row[cena]' and data_pri LIKE '$prihod_contol_row[data_pri]' and sklad LIKE '$prihod_contol_row[sklad]' and data_create_zap LIKE '$prihod_contol_row[data_create_zap]' and pol_create_zap LIKE '$prihod_contol_row[pol_create_zap]' and data_izm_zap LIKE '$prihod_contol_row[data_izm_zap]' and UPLOAD_DATE >= '$tomorrow'";
 $prihod_update_result = $db->query($prihod_update);
 if($prihod_update_result->num_rows == 2){
   echo "все хорошо <br/>";
@@ -68,7 +69,7 @@ elseif($prihod_update_result->num_rows == 1){
   //echo "id ".$prihod_contol_row["nomer"]." name -".$prihod_contol_row["name"]." cena -".$prihod_contol_row["cena"]." data_pri -".$prihod_contol_row["data_pri"]." kto sozdal -".$prihod_contol_row["pol_create_zap"]." kto sozdal -".$prihod_contol_row["data_izm_zap"]."<br/>";
 }
 else{
-  echo "Ошибка ";// необходимо создать таблицу для ошибок
+  echo "Ошибка <br/>";// необходимо создать таблицу для ошибок
 }
 }
 $add_prihod_upload_date="INSERT date_of_upload VALUES (NULL,'$today','11','prihod')";
@@ -93,7 +94,7 @@ if($rashod_data_upload_result->num_rows > 0){
 }
 else{
 
-$rashod_sql = "SELECT * FROM main_table WHERE UPLOAD_DATE='2023-09-21'";
+$rashod_sql = "SELECT * FROM main_table WHERE UPLOAD_DATE='2023-09-28'";
 $rashod_result= $db->query($rashod_sql);
 if($rashod_result->num_rows > 0) {
   while($rashod_result_row = $rashod_result->fetch_assoc()) {
@@ -101,8 +102,18 @@ if($rashod_result->num_rows > 0) {
     $rashod_update ="SELECT * FROM importxslx WHERE nomer='$rashod_result_row[nomer]' and cena LIKE '$rashod_result_row[cena]' and data_pri LIKE '$rashod_result_row[data_pri]' and sklad LIKE '$rashod_result_row[sklad]' and data_create_zap LIKE '$rashod_result_row[data_create_zap]' and pol_create_zap LIKE '$rashod_result_row[pol_create_zap]'";
     $rashod_update_result = $db->query($rashod_update);
     if($rashod_update_result->num_rows == 1){
+      while($rashod_update_result_row= $rashod_update_result->fetch_assoc()){
+        if($rashod_update_result_row['kolichestvo']==$rashod_result_row['kolichestvo']){
+          continue;
+        }
+        else{
 
-      echo "все четко <br/>";
+          $add_rashod = "INSERT rashod VALUES (NULL,'$rashod_update_result_row[kod_material]','$rashod_update_result_row[name]','$rashod_update_result_row[nomer]','$rashod_update_result_row[kolichestvo]','$rashod_update_result_row[ed_izm]','$rashod_update_result_row[cena]','$rashod_update_result_row[summa]','$rashod_update_result_row[data_pri]','$rashod_update_result_row[sklad]','$rashod_update_result_row[data_create_zap]','$today')";
+          $db->query($add_rashod);
+
+        $rashod_result_row['kolichestvo']-$rashod_update_result_row['kolichestvo']." опа опа оппапа<br/>";
+        }    
+      }  
     }
     else{
       //проверка изменения даты, если одинакове хорошо, если разные сначало отсортировать с по количеству потом отнять любой 
@@ -114,20 +125,6 @@ else{
   echo "Нет данных";
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //echo "id: " . $prihod_contol_row["nomer"]. " - Name: " . $prihod_contol_row["name"]. " --- " . $prihod_contol_row["data_pri"].  " --- " . $prihod_contol_row["sklad"]. " --- " . $prihod_contol_row["cena"]."<br>";
 
